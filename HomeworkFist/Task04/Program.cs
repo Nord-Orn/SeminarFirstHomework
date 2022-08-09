bf1c22850317d70ff8a151b9e5e1cb1794a270b3
«Andrey_Nord» <spglav@mail.ru>
2022-08-09 22:29:29 +0300

    add Task04 Задача №4

---
+++ HomeworkFist/Task04/Program.cs
@@ -0,0 +1,19 @@
+﻿// Задача 4: Напишите программу, которая принимает на вход три числа и выдаёт максимальное из этих чисел.
+Console.WriteLine("Введите необходимое колличество случайных чисел для сравнения");
+Console.Write("Колличество чисел для сравнения: ");
+int N = Convert.ToInt32(Console.ReadLine());
+int count = 0;
+int max = -1000;
+while (count < N)
+{
+    int number = new Random().Next(-1000, 1000);
+    Console.Write("число: ");
+    Console.WriteLine(number);
+    if (max < number)
+    {
+        max = number;
+    } 
+    count++;
+}
+Console.Write("Максимальное число из выборки: ");
+Console.WriteLine(max);

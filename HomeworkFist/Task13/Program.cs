513b4d82442388636eec3d72e10d88e30719e3ff
«Andrey_Nord» <spglav@mail.ru>
2022-08-12 00:49:43 +0300

    add Task 13 Задача №13

---
+++ HomeworkFist/Task13/Program.cs
@@ -0,0 +1,43 @@
+﻿// Задача 13: Напишите программу, которая выводит третью цифру 
+//заданного числа или сообщает, что третьей цифры нет.
+Console.Write("Ведите число: ");
+int number = Convert.ToInt32(Console.ReadLine());
+int Revers = 0;
+if (number >= 0)
+{
+    while (number > 0)
+    {
+        int A = number % 10;
+        Revers = (Revers * 10) + A;
+        number = number / 10;
+    }
+    Revers = Revers / 100 % 10;
+    if (Revers == 0)
+    {
+        Console.WriteLine("Третьей цифры нет");
+    }
+    else
+    {
+        Console.WriteLine($"Третья  цифра числа = {Revers}");
+    }
+}
+else if (number < 0)
+{
+    while (number < 0)
+    {
+        int A = number % 10;
+        Revers = (Revers * 10) - A;
+        number = number / 10;
+    }
+    Revers = Revers / 100 % 10;
+    if (Revers == 0)
+    {
+        Console.WriteLine("Третьей цифры нет");
+    }
+    else
+    {
+        Console.WriteLine($"Третья  цифра числа = {Revers}");
+    }
+}
+
+

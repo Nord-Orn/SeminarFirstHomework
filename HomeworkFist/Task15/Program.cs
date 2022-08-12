2441e5784dd2a733d3bff331f8648a6632c782b4
«Andrey_Nord» <spglav@mail.ru>
2022-08-12 23:25:26 +0300

    add  Task13 внес изменения

--- HomeworkFist/Task15/Program.cs
+++ HomeworkFist/Task15/Program.cs
@@ -2,6 +2,11 @@
 //обозначающую день недели, и проверяет, является ли этот день выходным.
 Console.WriteLine("Введите день недели: ");
 int day = Convert.ToInt32(Console.ReadLine());
+while (day < 1 || day > 7)
+{
+    Console.WriteLine("Число введено неверно. Ведите от 1 до 7");
+   day = Convert.ToInt32(Console.ReadLine());
+}
 switch(day)
 {	
     case 1:	
@@ -24,8 +29,5 @@ switch(day)
         break;
     case 7:
         Console.WriteLine("Воскресенье-выходной день! Ура!!!");
-        break;
-    default:
-        Console.WriteLine("Нет такого дня недели");
         break;
 }

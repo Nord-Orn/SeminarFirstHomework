4d844764a4195e17239661863122fcfb84469ac8
«Andrey_Nord» <spglav@mail.ru>
2022-08-14 21:03:20 +0300

    add Task23 Задача №23

---
+++ HomeworkFist/Task23/Program.cs
@@ -0,0 +1,14 @@
+﻿// Задача 23 Напишите программу, которая принимает на вход число (N) и выдаёт таблицу кубов
+Console.Write("Ведите последнее число таблицы: ");
+double N = Convert.ToDouble(Console.ReadLine());
+Console.Write("Ведите интересующую степень: ");
+double A = Convert.ToDouble(Console.ReadLine());
+double count = 0;
+double step = 0;
+while (count <= N)
+{
+    step = Convert.ToDouble(Math.Pow(count, A));
+    Console.WriteLine($"Число = {count} в степени {A} = {step}");
+    count++;
+}
+

ea384f63c4a8c841c61157d73ff479e2508dd981
«Andrey_Nord» <spglav@mail.ru>
2022-08-14 19:19:00 +0300

    add Task19 Задача №19

---
+++ HomeworkFist/Task19/Program.cs
@@ -0,0 +1,57 @@
+﻿//Задача 19. Напишите программу, которая принимает на вход пятизначное число 
+//и проверяет, является ли оно палиндромом.
+//
+Console.WriteLine("Введите пятизначное число");
+int number = Convert.ToInt32(Console.ReadLine());
+int numberA = 0;
+int revers = 0;
+int remains = 0;
+int numberB =0;
+if (number > 0)
+{
+    while (number > 99999 || number < 10000)
+    {
+        Console.WriteLine("Ошибка. Введите пятизначное число");
+        number = Convert.ToInt32(Console.ReadLine());
+    }
+    numberA = number % 100;
+    while (number > 0)
+    {
+        remains = number % 10;
+        revers = (revers * 10) + remains;
+        number = number / 10;
+    }
+    numberB = revers % 100;
+    if (numberA == numberB)
+    {
+        Console.Write("Палиндром.");
+    }
+    else
+    {
+        Console.WriteLine("Не палиндром.");
+    }
+}
+else if (number < 0)
+{
+    while (number < -99999 || number > -10000)
+    {
+        Console.WriteLine("Ошибка. Введите пятизначное число");
+        number = Convert.ToInt32(Console.ReadLine());
+    }
+    numberA = number % 100;
+    while (number < 0)
+    {
+        remains = number % 10;
+        revers = (revers * 10) - remains;
+        number = number / 10;
+    }
+    numberB = revers % 100*-1;
+    if (numberA == numberB)
+    {
+        Console.WriteLine("Палиндром.");
+    }
+    else
+    {
+        Console.WriteLine("Не палиндром.");
+    }
+}

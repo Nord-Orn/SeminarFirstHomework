f4de14ba512cea179d00c4e285d44d363ead34fa
«Andrey_Nord» <spglav@mail.ru>
2022-08-19 20:36:12 +0300

    add Task29 внес  изменения если max <= min

--- HomeworkFist/Task29/Program.cs
+++ HomeworkFist/Task29/Program.cs
@@ -9,12 +9,18 @@ int[] arrey(int number, int min, int max)
     return arr;
 }
 
+Console.Clear();
 Console.WriteLine("Введите колличество элементов массива");
 int number = Convert.ToInt32(Console.ReadLine());
 Console.WriteLine("Введите минимальное значение элементов массива");
 int min = Convert.ToInt32(Console.ReadLine());
 Console.WriteLine("Введите максимальное значение элементов массива");
 int max = Convert.ToInt32(Console.ReadLine());
+while (max <= min)
+{
+    Console.WriteLine("Ошибка. Введите число больше минимального");
+    max = Convert.ToInt32(Console.ReadLine());
+}
 Console.Write("Массив: ");
 Console.WriteLine(string.Join(", ", arrey(number, min, max)));
 Console.WriteLine();

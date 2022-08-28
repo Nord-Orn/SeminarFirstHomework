b99b8e773dd1206c09c36e3169e7b8e12d3941e5
«Andrey_Nord» <spglav@mail.ru>
2022-08-28 18:35:39 +0300

    add Task45Rekurs Задача с рекурсией

---
+++ HomeworkFist/Task45Rekurs/Program.cs
@@ -0,0 +1,15 @@
+﻿// Дополнительная задача на рекурсию
+//Напишите программу, которая принимает на вход число N и выдаёт произведение чисел от 1 до N. (использовать рекурсию)
+//4 -> 24
+//5 -> 120
+double Factorial(int N)
+{
+    //if (N == 0) return 1; нарушает рекурсию, вписываем в условие.
+    if (N == 1) return 1;
+    else return N * Factorial(N-1);
+}
+
+Console.Write("Введите факториал: ");
+int N = Convert.ToInt32(Console.ReadLine());
+if (N==0) Console.WriteLine($"{N}! = 1");
+else Console.WriteLine($"{N}! = {Factorial(N)}");

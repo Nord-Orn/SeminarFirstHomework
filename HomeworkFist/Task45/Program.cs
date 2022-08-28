bfd445be4d5dfd7e9cf301cc6350819f0288bae0
«Andrey_Nord» <spglav@mail.ru>
2022-08-28 16:38:42 +0300

    add Task45

---
+++ HomeworkFist/Task45/Program.cs
@@ -0,0 +1,17 @@
+﻿// Задача 45: Напишите программу, которая будет создавать 
+//копию заданного массива с помощью поэлементного копирования.
+Console.Clear();
+Console.Write("Введите размерность исходного массива: ");
+int N = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine();
+int[] array = new int[N];
+int[] copy = new int[array.Length];
+for (int i = 0; i < N; i++)
+{
+    array[i] = new Random().Next(-100, 101);
+    copy[i] = array[i];
+}
+Console.WriteLine("Исхожный массив: [" + string.Join(", ", array) + "]");
+Console.WriteLine();
+Console.WriteLine("Скопированный массив: [" + string.Join(", ", copy) + "]");
+Console.WriteLine();

95ea80e336e0af7127d8c2cb1fc405ec67244842
«Andrey_Nord» <spglav@mail.ru>
2022-08-28 17:21:31 +0300

    add Task45addit Дополнительная задача

---
+++ HomeworkFist/Task45addit/Program.cs
@@ -0,0 +1,22 @@
+﻿// Дополнительная задача на рекурсию
+//Напишите программу, которая принимает на вход число N и выдаёт произведение чисел от 1 до N. (использовать рекурсию)
+//4 -> 24
+//5 -> 120
+Console.Clear();
+Console.Write("Введите факториал: ");
+int N = Convert.ToInt32(Console.ReadLine());
+if (N == 0)
+{
+    Console.WriteLine($"{N}! = 1");
+}
+else
+{
+    double number = 1;
+    for (int i = 1; i < N + 1; i++)
+    {
+        number = number * i;
+    }
+    Console.WriteLine($"{N}! = {number}");
+    Console.WriteLine();
+}
+
